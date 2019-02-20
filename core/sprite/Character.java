fe04dfc9b37f1e8165120da0bb653d20b1bc8d2c
BrettPaffrath <brett@tremar.ca>
2019-02-20 15:46:02 -0700

    Update Character.java

---
+++ core/sprite/Character.java
@@ -0,0 +1,76 @@
+abstract public class Character extends Sprite {
+    
+    private String name;
+    private double gravity;
+    private int health;
+    private int action = 0;
+    private boolean direction; 
+    private double speed;
+
+    public Character(String n, int heal, Sprite spri, double spe) {
+        super(spri);
+        name = n;
+        health = h;
+        speed = spe;
+    }
+
+    public void dead() {
+
+    }
+
+    public boolean isDead() {
+        return (health <= 0);
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int h) {
+        health = h;
+    }
+
+    public double getGravity() {
+        return gravity;
+    }
+
+    public void setGravity(double g) {
+        gravity = g;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int d) {
+        direction = d;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double s) {
+        speed = s;
+    }
+
+    public void run() {
+        if (!isColliding(this)) {
+
+            //True = Right, False = Left
+            if (direction) {
+                Coordinate moveTo = new Coordinate(this.x+1, this.y);  
+                super.setCoordinate(moveTo);   
+            } else {
+                Coordinate moveTo = new Coordinate(this.x-1, this.y);
+                super.setCoordinate(moveTo); 
+            }
+        }
+    }
+
+    public void attack() {
+
+    } 
+
+   
+}
