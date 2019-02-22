8a405c12c412cd66d2201ec49e3dae12f06890cf
Cloudy Young <2598508898@qq.com>
2019-02-22 00:17:39 -0700

    GameTerminal

--- core/GameTerminal.java
+++ core/GameTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class GameTerminal{
 	
+	private static String display;
 
+	public static void print(){
+		System.out.println(display);
+	}
+
+	private static boolean isUpdated(){
+		return !(getDisplay().equals(display));
+	}
+
+	public static String getDisplay(){
+		return GameState.getCurrentGameMap().generateMapTerminal();
+	}
+
+	private static terminalTimer(){
+		Timer timer = new Timer();
+
+		TimerTask task = new TimerTask() {   
+			if(){
+
+			}
+		};
+	}
 	
 }
