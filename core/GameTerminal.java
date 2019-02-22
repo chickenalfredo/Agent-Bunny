cc017610502be1c73b46bc566fa43c5deffcb840
Cloudy Young <2598508898@qq.com>
2019-02-22 02:12:39 -0700

    Add javadoc

--- core/GameTerminal.java
+++ core/GameTerminal.java
@@ -8,27 +8,42 @@ public class GameTerminal{
     
     private static String display;
 
+    /**
+	* Print string to the terminal
+    */
     public static void print(){
         System.out.println(display);
     }
 
+    /**
+	* check if the string is updated
+	* @return boolean
+    */
     private static boolean isUpdated(){
         return !(getDisplay().equals(display));
     }
 
+    /**
+    * Get the display string
+	* @return String
+    */
     public static String getDisplay(){
         return GameState.getCurrentGameMap().generateMapTerminal();
     }
 
-    private static void terminalTimer(){
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {   
-            public void run() {   
+    /**
+	* This is a terminal timer which will refresh each second
+    */
+    public static void terminalTimer(){
+    	TimerTask task = new TimerTask(){
+            public void run() {
             	if(isUpdated()){
             		print();
             	}
-            }   
-    };
+            }
+		};
+        Timer timer = new Timer();
+        timer.schedule(task, 0, 1000);
     }
     
 }
