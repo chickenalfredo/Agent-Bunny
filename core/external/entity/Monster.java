c55d7f34d605cb0bdc72f53702095c74014dd7e5
Harry Lee <harold.d.lee@gmail.com>
2019-02-23 13:56:44 -0700

    Update Monster.java

--- core/external/entity/Monster.java
+++ core/external/entity/Monster.java
@@ -1,12 +1,41 @@
 import java.util.Random;
+import java.lang.Thread;
 
 /**
- * This class implements a weak hostile npc in the game.
+ * This class implements the AI and behaviours of weak hostile npc in the game.
+ * It is the extension of the Entity class.
  * 
  * @author Harry Lee
  */
-public class Monster extends Character {
+public class Monster extends Entity {
     private int power = 20;
+
+    /**
+     * This is the constructor for this class that extends from Entity class.
+     * 
+     * @param entityName   This is the entity's name.
+     * @param entityHealth This is the entity's health.
+     * @param entitySpeed  This is the entity's speed.
+     * @param enemy        This detemines whether if the entity is an enemy.
+     * @param entityX      This is the x coordinate of the location of the entity.
+     * @param entityY      This is the y coordinate of the location of the entity.
+     * @param entityWidth  This is the width of the entity's size.
+     * @param entityHeight This is the height of the entity's size.
+     * @param power        This sets the overall strength of the enemy's attack.
+     */
+    public Monster(String entityName, int entityHealth, double entitySpeed, boolean enemy, float entityX, float entityY,
+            float entityWidth, float entityHeight, int power) {
+        super(entityName, entityHealth, entitySpeed, enemy, entityX, entityY, entityWidth, entityHeight);
+        setPower(power);
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int strength) {
+        this.power = strength;
+    }
 
     /**
      * This generates a random integer between a range of numbers for some
@@ -31,16 +60,18 @@ public class Monster extends Character {
     }
 
     /**
-     * This is what the monster will do when player is not nearby.
+     * This is what the monster will passively do when player is not nearby.
      */
     public void idle() {
-        while (isDead() == false) {
-            int waitTime = getRandomInt(4000, 8000);
-            int directionNum = getRandomInt(0, 1);
-            int randNum = getRandomInt(50, 200);
-            setDirection(directionNum);
-            for (int i = 0; i <= randNum; i++) {
-                run();
+        if (!action) {
+            while (!isDead()) {
+                int waitTime = getRandomInt(4000, 8000);
+                int directionNum = getRandomInt(0, 1);
+                int randNum = getRandomInt(50, 200);
+                setDirection(directionNum);
+                for (int i = 0; i <= randNum; i++) {
+                    run();
+                }
                 sleep(waitTime);
             }
 
@@ -51,7 +82,7 @@ public class Monster extends Character {
      * This sets monster's power when attacking.
      */
     public void attack() {
-        Sprite.changeHP(power * -1);
+        Sprite.Player.setHealth(power * -1);
     }
 
     /**
@@ -59,26 +90,30 @@ public class Monster extends Character {
      * towrds the player and attack.
      */
     public void charge() {
-        List<Sprite> list = see();
-        for (Sprite each : list) {
-            if (each instanceof Player) {
-                if (each.getCoordinate().x() <= Monster.x) {
-                    setDirection(0);
-                    while (isDead() == false) {
-                        while (Monster.x - each.getCoordinate().x() >= 20) {
-                            run();
+        if (!action) {
+            action = true;
+            List<Sprite> list = see();
+            for (Sprite each : list) {
+                if (each instanceof Player) {
+                    if (each.getCoordinate().getx() <= Monster.x) {
+                        setDirection(false);
+                        while (isDead() == false) {
+                            while (this.Monster.getx() - each.getCoordinate().getx() >= 20) {
+                                run();
+                            }
+                            attack();
                         }
-                        attack();
-                    }
-                } else {
-                    setDirection(1);
-                    while (isDead() == false) {
-                        while (each.getCoordinate().x() - Monster.x >= 20) {
-                            run();
+                    } else {
+                        setDirection(true);
+                        while (isDead() == false) {
+                            while (each.getCoordinate().getx() - this.Monster.getx() >= 20) {
+                                run();
+                            }
+                            attack();
                         }
-                        attack();
                     }
                 }
+                action = false;
             }
         }
     }
