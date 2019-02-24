133fcb21d1425c93fa8612bc472f0b63b53d502b
BrettPaffrath <brett@tremar.ca>
2019-02-24 16:35:20 -0700

    Update WhiteBlock.java

--- src/main/java/core/external/tile/WhiteBlock.java
+++ src/main/java/core/external/tile/WhiteBlock.java
@@ -7,5 +7,7 @@ public class WhiteBlock extends Tile {
     public WhiteBlock(char terminalChar, float x, float y, float width, float height) {
         super(terminalChar, x, y, width, height);
         setIsCollidable(true);
+
+        
     }
 }
