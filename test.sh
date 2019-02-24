1b37c132a670c362c3a0843c74c0b4d4440bd5c7
DanielContreras <contrerass.daniel@gmail.com>
2019-02-23 17:32:50 -0700

    Remove all unused folders

--- test.sh
+++
@@ -1,6 +0,0 @@
-# Script to automatically display results of program
-# Run chmod +x test.sh
-
-rm -rf bin/*
-javac -d target/classes -classpath target/dependencies/gson-2.8.5.jar -sourcepath core/sprite core/sprite/*.java
-java -classpath target/classes:target/dependencies/gson-2.8.5.jar core.sprite.Main
