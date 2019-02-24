b469d85ad92bbb31688f7b0ad56f5965eb3abbf9
DanielContreras <contrerass.daniel@gmail.com>
2019-02-24 00:52:44 -0800

    Fixed execution

--- src/main/java/core/App.java
+++ src/main/java/core/App.java
@@ -4,9 +4,7 @@ import core.sprite.Sprite;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         Sprite s = new Sprite();
         System.out.println(s.toString());
-        
     }
 }
