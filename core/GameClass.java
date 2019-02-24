981d4b70f33d8ea504726d3f76265fe2062951f9
Cloudy Young <2598508898@qq.com>
2019-02-23 21:55:21 -0700

    GameClass and GameData edited

--- core/GameClass.java
+++ core/GameClass.java
@@ -1,32 +1,39 @@
 package core;
 
 
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.lang.Class;
+import java.lang.reflect.InvocationTargetException;
 
 public class GameClass<T>{
 
-	Class<T> classObj;
-	Constructor<T> constructorObj;
-	
-	public GameClass<T>(Class<T> class){
-		this.classObj = class;
-		return this.class;
-	}
-
-	public Object<T> getConstructor(){
-		this.constructorObj = this.classObj.getConstructor();
-        Object<T> instanceConstructor = this.constructorObj.newInstance();
-		return instanceConstructor;
-	}
-
-	public Object<?> invokeMethod(String method){
-		Method method = this.classObj.getMethod("sayHello");
-        return method.invoke(method);
-	}
-
+    Class<T> classObj;
+    Constructor<T> constructorObj;
+    
+    public GameClass(Class<T> classObj){
+        this.classObj = classObj;
+    }
+    
+    /**
+     * @link https://www.sczyh30.com/posts/Java/java-reflection-1/
+     */
+    public Object getConstructor() throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        this.constructorObj = this.classObj.getConstructor();
+        Object instanceConstructor = this.constructorObj.newInstance();
+        return instanceConstructor;
+    }
+    
+    /**
+     * @link https://www.sczyh30.com/posts/Java/java-reflection-1/
+     */
+    public Object invokeMethod(String method) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        Method methodObj = this.classObj.getMethod("sayHello");
+        return methodObj.invoke(method);
+    }
+    
 
 }
--- core/GameData.java
+++ core/GameData.java
@@ -3,6 +3,11 @@ package core;
 
 import java.io.*;
 import java.util.List;
+import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import core.map.GameMap;
 
 
 /**
@@ -19,7 +24,31 @@ public class GameData{
 	private Map<String, Map> gameCharacterRecord;
 
 	public static void loadGameMap(){
+		String path = "map/";
+		List<String> fileList = getFileNamesByPath(path);
 
+		for(int t = 0; t < fileList.size(); t ++){
+			String fileName = fileList.get(t);
+			System.out.println(fileName);
+			//GameClass mapClass = GameUtility.importClass(fileName, "Level.java");
+		}
+
+	}
+
+
+	private static ArrayList<String> getFileNamesByPath(String path){
+		ArrayList<String> files = new ArrayList<String>(); 
+		File file = new File(path); 
+		File[] tempList = file.listFiles(); 
+		for (int i = 0; i < tempList.length; i++){
+			if (tempList[i].isFile()){
+			 	files.add(tempList[i].toString());
+			}
+			if(tempList[i].isDirectory()){
+				// Do not include subFolder, no action
+			}
+		} 
+		return files;
 	}
 
 }
