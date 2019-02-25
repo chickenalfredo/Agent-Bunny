9ad0ae4476f3182407bfc6a9189b15cb8be4162b
DanielContreras <contrerass.daniel@gmail.com>
2019-02-24 19:39:43 -0700

    Add JUnit skeleton class files

---
+++ src/test/java/core/external/props/DiamondTest.java
@@ -0,0 +1,20 @@
+package core.external.props;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class DiamondTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/core/map/GameMapTest.java
@@ -0,0 +1,20 @@
+package core.map;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GameMapTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
