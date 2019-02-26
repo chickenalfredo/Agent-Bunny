6be143024e4e1f279cc7500ad7d6fe0e2248d2be
DanielContreras <contrerass.daniel@gmail.com>
2019-02-26 01:49:57 -0800

    Add unit test for Collision

--- src/test/java/core/sprite/CollisionTest.java
+++ src/test/java/core/sprite/CollisionTest.java
@@ -1,9 +1,9 @@
 package core.sprite;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import core.sprite.*;
 
 public class CollisionTest 
 {
@@ -12,49 +12,104 @@ public class CollisionTest
     public void testIntersection() 
     {
         Collision c = new Collision();
-        assertTrue(c.intersection());
+        assertFalse(c.intersection());
     }
 
     @Test
     public void testSweep() 
     {
         Collision c = new Collision();
-        assertTrue(c.sweep());
+        assertFalse(c.sweep());
     }
 
     @Test
     public void testIntersectPoint() 
     {
         Collision c = new Collision();
-        assertTrue(c.intersectPoint(new Coordinate(1, 1)));
+        assertFalse(c.intersectPoint(new Coordinate(1, 1)));
     }
 
     @Test
     public void testIntersectRaySegment() 
     {
         Collision c = new Collision();
-        assertTrue(c.intersectRaySegment(new Coordinate(1,2), new Coordinate(2,2), 1, 1));
+        assertFalse(c.intersectRaySegment(new Coordinate(1,2), new Coordinate(2,2), 1, 1));
     }
 
     @Test
-    public void testIntersectAABB() 
+    public void test_intersectAABB_from_the_left_with_no_collision() 
     {
         Collision c = new Collision();
-        assertTrue(c.intersectAABB(new Sprite(), new Sprite()));
+        assertFalse(c.intersectAABB(new Sprite('a', 15, 20, 5, 5), new Sprite('b', 10, 20, 5, 5)));
+    }
+
+    @Test
+    public void test_intersectAABB_from_the_top_with_no_collision() 
+    {
+        Collision c = new Collision();
+        assertFalse(c.intersectAABB(new Sprite('a', 15, 20, 5, 5), new Sprite('b', 15, 25, 5, 5)));
+
+    }
+
+    @Test
+    public void test_intersectAABB_from_the_right_with_no_collision() 
+    {
+        Collision c = new Collision();
+        assertFalse(c.intersectAABB(new Sprite('a', 15, 20, 5, 5), new Sprite('b', 20, 20, 5, 5)));
+
+    }
+
+    @Test
+    public void test_intersectAABB_from_the_bottom_with_no_collision() 
+    {
+        Collision c = new Collision();
+        assertFalse(c.intersectAABB(new Sprite('a', 15, 20, 5, 5), new Sprite('b', 15, 15, 5, 5)));
+
+    }
+
+    @Test
+    public void test_intersectAABB_from_the_left_with_collision() 
+    {
+        Collision c = new Collision();
+        assertTrue(c.intersectAABB(new Sprite('a', 15, 20, 5, 5), new Sprite('b', 11, 20, 5, 5)));
+    }
+
+    @Test
+    public void test_intersectAABB_from_the_top_with_collision() 
+    {
+        Collision c = new Collision();
+        assertTrue(c.intersectAABB(new Sprite('a', 15, 20, 5, 5), new Sprite('b', 15, 24, 5, 5)));
+
+    }
+
+    @Test
+    public void test_intersectAABB_from_the_right_with_collision() 
+    {
+        Collision c = new Collision();
+        assertTrue(c.intersectAABB(new Sprite('a', 15, 20, 5, 5), new Sprite('b', 19, 20, 5, 5)));
+
+    }
+
+    @Test
+    public void test_intersectAABB_from_the_bottom_with_collision() 
+    {
+        Collision c = new Collision();
+        assertTrue(c.intersectAABB(new Sprite('a', 15, 20, 5, 5), new Sprite('b', 15, 16, 5, 5)));
+
     }
 
     @Test
     public void testSweptAABB() 
     {
         Collision c = new Collision();
-        assertTrue(c.sweptAABB(new Sprite(), new Coordinate(2,2)));
+        assertFalse(c.sweptAABB(new Sprite(), new Coordinate(2,2)));
     }
 
     @Test
     public void testSweepInto() 
     {
         Collision c = new Collision();
-        assertTrue(c.sweepInto(new Sprite(), new Coordinate());
+        assertFalse(c.sweepInto(new Sprite(), new Coordinate()));
     }
     
 }
