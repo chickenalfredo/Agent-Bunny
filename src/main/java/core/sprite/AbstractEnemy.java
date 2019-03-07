79c1d4c32c5697a7980e87d43fb9b2e403c7221c
DanielContreras <contrerass.daniel@gmail.com>
2019-03-06 21:29:57 -0800

    Add abstract enemy class skeleton

--- src/main/java/core/sprite/AbstractEnemy.java
+++ src/main/java/core/sprite/AbstractEnemy.java
@@ -5,7 +5,7 @@ import core.sprite.interfaces.IEnemyDefensive;
 public abstract class AbstractEnemy extends Entity implements IEnemyDefensive {
 
     public AbstractEnemy(double x, double y, double width, double height, char playerImage, String playerName, int playerHealth, double playerSpeed) {
-        super(playerName, playerHealth, playerSpeed, true, x, y, width, height, playerImage);
+        super(playerName, playerHealth, playerSpeed, false, x, y, width, height, playerImage);
     }
 
 }
