705ea38a43a6bfcc18a2902b1bf0ef4ff293aa0f
BrettPaffrath <brett@tremar.ca>
2019-03-13 23:28:41 -0600

    Fixed Endpoint png and position

--- src/main/java/core/external/level/Chapter1Level1.java
+++ src/main/java/core/external/level/Chapter1Level1.java
@@ -20,7 +20,7 @@ public class Chapter1Level1 extends GameMap {
 
 		Monster monster = new Monster(1000, 200, 100, 100, "src/main/resources/assets/Beetle.png");
 		BlackWolf wolf = new BlackWolf(200.0, 300.0, 100, 100, "src/main/resources/assets/Cyclops.png");
-		EndPoint ep = new EndPoint(50.0, 600.0, 100, 100, "src/main/resources/assets/Door.png");
+		EndPoint ep = new EndPoint(1200, 720, 100, 175, "src/main/resources/assets/Door.png");
 		Grass block1 = new Grass(500.0, 350.0, 100, 100, "src/main/resources/assets/Grass.png");
 		Wall wall = new Wall(0, ScreenBuilder.getPrimaryScreenBounds().getHeight() - 150, ScreenBuilder.getPrimaryScreenBounds().getWidth(), 150, "src/main/resources/assets/Grass.png");
 
