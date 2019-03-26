e7fb1988c244fac8167391a51ad5f6c3124d1321
Daniel Contreras <DanielContreras@users.noreply.github.com>
2019-03-26 00:23:56 -0600

    Feature animation (#130)

--- src/main/java/core/command/commands/AttackCommand.java
+++ src/main/java/core/command/commands/AttackCommand.java
@@ -1,7 +1,10 @@
 package core.command.commands;
 
+import core.animations.Animation;
 import core.command.Command;
+import core.ecs.components.RenderComponent;
 import core.external.entity.Hero;
+import core.sprite.Sprite;
 import core.sprite.World;
 
 /**
@@ -17,6 +20,7 @@ public class AttackCommand extends Command {
      */
     public AttackCommand(boolean isKeyPressed) {
         this.isKeyPressed = isKeyPressed;
+
     }
 
     /**
@@ -26,7 +30,15 @@ public class AttackCommand extends Command {
      */
     @Override
     public void execute(Hero actor, World world) {
-        if (isKeyPressed)
+        if (isKeyPressed) {
             actor.attackCollider(world);
+            world.getHero().getComponent("RenderComponent", RenderComponent.class).setViewPort(world.getHero().getComponent("RenderComponent", RenderComponent.class).getAnimationSystem().animate(
+                world.getHero().getComponent("RenderComponent", RenderComponent.class).getImage(), (Sprite) actor,
+                "attack"));
+        } else {
+            world.getHero().getComponent("RenderComponent", RenderComponent.class).getAnimationSystem().animate(
+                world.getHero().getComponent("RenderComponent", RenderComponent.class).getImage(), (Sprite) actor,
+                "idle");
+        }
     }
 }
