7ae14070cf75f3acf76c749b5e264d1998c876ae
DanielContreras <contrerass.daniel@gmail.com>
2019-03-26 01:29:34 -0600

    Fix build error

--- src/main/java/core/sprite/World.java
+++ src/main/java/core/sprite/World.java
@@ -3,7 +3,6 @@ package core.sprite;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-import core.animations.Animation;
 import core.external.entity.Hero;
 import core.external.level.Chapter1Level1;
 import core.map.GameMap;
@@ -14,7 +13,6 @@ public class World implements Serializable {
     private static final long serialVersionUID = -593390538413494469L;
     private ArrayList<Sprite> m_entities = new ArrayList<Sprite>();
     private Hero hero;
-    private Animation animationSystem;
     private GameMap level;
 
     /**
